e03a666b72993e8758a8f8e7d132dc1c138dfbe4
Antony Gerhold <162134200+coffeemate22@users.noreply.github.com>
2024-03-04 15:15:11 +0200

    Update codeing.py

--- codeing.py
+++ codeing.py
@@ -19,3 +19,11 @@ if __name__ == "__main__":
             item: data["data"]["children"][id_]["data"][item] for item in wanted_data
         }
     return data_dict#nice
+  while True:
+        isbn = input("\nEnter the ISBN code to search (or 'quit' to stop): ").strip()
+        if isbn.lower() in ("", "q", "quit", "exit", "stop"):
+            break
+
+        if len(isbn) not in (10, 13) or not isbn.isdigit():
+            print(f"Sorry, {isbn} is not a valid ISBN.  Please, input a valid ISBN.")
+            continue
